8460a487c6bc2889f5b880f0f0323a403b92ea94
ruslana <ruslana.prus@ukr.net>
2024-09-05 18:58:29 +0200

    add custom date formatter

--- src/main/java/org/example/service/DatabaseQueryService.java
+++ src/main/java/org/example/service/DatabaseQueryService.java
@@ -4,6 +4,8 @@ import org.example.db.ConnectionManager;
 import org.example.db.SQLExecutor;
 import org.example.viewmodel.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -74,8 +76,10 @@ public class DatabaseQueryService {
         }
     }
 
-    public Optional<List<YoungestEldestWorker>> findYoungestEldestWorker (String sqlFilePath) {
+    public Optional<List<YoungestEldestWorker>> findYoungestEldestWorker(String sqlFilePath) {
         String errorMessage = "Failed to execute findYoungestEldestWorker query";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
         try (SQLExecutor executor = new SQLExecutor(connectionManager.getConnection())) {
             return executor.executeQuery(
@@ -84,7 +88,7 @@ public class DatabaseQueryService {
                     rs -> new YoungestEldestWorker(
                             rs.getString("type"),
                             rs.getString("name"),
-                            rs.getDate("birthday").toLocalDate()
+                            LocalDate.parse(rs.getString("birthday"), formatter)
                     )
             );
         }
